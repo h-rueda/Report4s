729f42606d5f6aca42a3da4efc67f1332a16bba4
Harmin <harmin.parra-rueda@atos.net>
2023-08-30 00:56:07 +0200

    delete unwanted backup files

--- zip.sh
+++
@@ -1,5 +0,0 @@
-file=report4s-5.0
-rm ${file}.zip
-cp jar/${file}.jar lib/
-zip ${file}.zip CHANGELOG LICENSE README* report4s.properties lib/commons-io-*.jar lib/commons-lang3-*.jar lib/report4s-*.jar -r javadoc
-rm lib/${file}.jar
