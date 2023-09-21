3f0cc3b1c08a566256536319df3e158af8706c74
Harmin <harmin.parra-rueda@atos.net>
2023-09-21 23:02:15 +0200

    Set driver log output file

--- test/com/github/report4s/test/TemplateTest.java
+++ test/com/github/report4s/test/TemplateTest.java
@@ -1,30 +1,33 @@
 package com.github.report4s.test;
 
-import java.io.*;
-import java.net.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.FileSystems;
 import java.util.Properties;
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.support.events.*;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.testng.annotations.*;
-
 import com.github.report4s.DriverListener;
+
 
 public class TemplateTest {
 
 	protected WebDriver driver;
-	protected String page = "file://" +
-	            FileSystems.getDefault()
-	            .getPath("test", "page.html")
-	            .toAbsolutePath()
-	            .toString();
+	protected String page = "file://" + FileSystems.getDefault()
+	            .getPath("test", "page.html").toAbsolutePath().toString();
+	private String logs_dir = FileSystems.getDefault().getPath("logs").toAbsolutePath().toString();
 
+	
 	@BeforeSuite(alwaysRun=true)
 	public void setUp() throws MalformedURLException {
 		WebDriver raw_driver = null;
@@ -33,6 +36,9 @@ public class TemplateTest {
   		System.setProperty("webdriver.gecko.driver", "/home/harmin/eclipse/webdrivers/geckodriver");
 		System.setProperty("webdriver.chrome.driver", "/home/harmin/eclipse/webdrivers/chromedriver");
   		System.setProperty("webdriver.edge.driver", "/home/harmin/eclipse/webdrivers/msedgedriver");
+  		System.setProperty("webdriver.firefox.logfile", this.logs_dir + "/geckodriver.log");
+        System.setProperty("webdriver.chrome.logfile", this.logs_dir + "/chromedriver.log");
+        System.setProperty("webdriver.edge.logfile", this.logs_dir + "/msedgedriver.log");
 		try {
 			input = new FileInputStream("browser.properties");
 			Properties prop = new Properties();
@@ -50,23 +56,27 @@ public class TemplateTest {
 		if(browser.equals("firefox")) {
 		    FirefoxOptions options = new FirefoxOptions();
 		    options.addArguments("--headless");
-			raw_driver = new FirefoxDriver(options);
+            GeckoDriverService service = GeckoDriverService.createDefaultService();
+			raw_driver = new FirefoxDriver(service, options);
 		}
 		if(browser.equals("edge")) {
             EdgeOptions options = new EdgeOptions();
             options.addArguments("--headless");
-			raw_driver = new EdgeDriver(options);
+            EdgeDriverService service = EdgeDriverService.createDefaultService();
+			raw_driver = new EdgeDriver(service, options);
 		}
 		if(browser.equals("chrome")) {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
-			raw_driver = new ChromeDriver(options);
+            ChromeDriverService service = ChromeDriverService.createDefaultService();
+			raw_driver = new ChromeDriver(service, options);
 		}
 		DriverListener listener = new DriverListener();
 		this.driver = new EventFiringDecorator<WebDriver>(listener).decorate(raw_driver);
 		this.driver.manage().window().maximize();
 	}
-	
+
+
 	@AfterSuite
 	public void tearDown() {
 		try { this.driver.quit(); }
