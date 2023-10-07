eb32a25bff4e86fc013d8a37b98644c1c3108a19
Harmin <harmin.parra-rueda@atos.net>
2023-10-08 00:16:58 +0200

    update version

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
-file=report4s-5.1
+file=report4s-5.2
 rm ${file}.zip
 cp jar/${file}.jar lib/
 zip ${file}.zip CHANGELOG LICENSE README* report4s.properties lib/commons-io-*.jar lib/commons-lang3-*.jar lib/report4s-*.jar -r javadoc
