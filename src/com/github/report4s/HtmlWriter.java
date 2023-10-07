f4761b290da42dc27359c8412ee2548b25881d17
Harmin <harmin.parra-rueda@atos.net>
2023-10-07 21:15:41 +0200

    Remove unused name attribute in test step table row

--- src/com/github/report4s/HtmlWriter.java
+++ src/com/github/report4s/HtmlWriter.java
@@ -169,7 +169,7 @@ class HtmlWriter {
             "            <tr>" + "\n" +
             "                <th style=\"width:100px\">Status</th>" + "\n" +
             "                <th>Description</th>" + "\n" +
-            "                <th style=\"width:90px\" name=\"screenshot_test_" + Listeners.methodCount + "\">Screenshot</th>" + "\n";
+            "                <th style=\"width:90px\">Screenshot</th>" + "\n";
         if (Report4s.pagesource)
             table_header += 
             "                <th style=\"width:60px\">Source</th>" + "\n";
